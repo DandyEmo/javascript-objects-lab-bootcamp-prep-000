92e4cbb652394b41f61d3101c1c7b9c1077dbd35
kevin Abreu <kabreu11385@gmail.com>
2019-08-14 20:19:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
    
 }
 function destructivelyUpdateObjectWithKeyAndVal(object, key1, value1) {
-      var object = 
+      var object = {}
 }
 
